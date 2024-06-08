8b06f20f281d5e1eebca56133c6cd5fdcc9186fd
John Paul Al-haddi <jpaul.alhaddi@gmail.com>
2024-06-09 01:11:17 +0800

    Add: mongoose

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,3 @@
-    export const PORT = 5555;
+export const PORT = 5555;
+    
+export const mongoDBURL = 'mongodb+srv://jpaulalhaddi:KFb0EBEGyDlMG6f2@book-store-mern.h98oqtb.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN'
