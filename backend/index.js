e9c44874df2c962280d4bdbe257110b2e0a70f27
John Paul Al-haddi <jpaul.alhaddi@gmail.com>
2024-06-09 03:41:55 +0800

    Add: Delete a book

--- backend/index.js
+++ backend/index.js
@@ -93,6 +93,23 @@ app.put('/books/:id', async (request, response) => {
     }
 });
 
+// Route for delete a book
+app.delete('/books/:id', async (request, response) => {
+    try{
+        const { id } = request.params;
+        const result = await Book.findByIdAndDelete(id);
+        
+        if (!result){
+            return response.status(404).json({ message: 'Book not found'});
+        }
+        return response.status(200).json({ message: 'Book updated successfully'});
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+    }
+});
+
+
 mongoose
     .connect(mongoDBURL)
     .then(() => {
